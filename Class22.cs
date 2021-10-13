3bd1867ce45437a37f161a019c20d500952612b6
SwiftKillers <yuri.silenok@gmail.com>
2021-10-13 08:37:22 +0300

    вв

---
+++ Class22.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Class22
+    {
+        int ch;
+        int z;
+        public Class22(int x, int y)
+        {
+            ch = x; z = y;
+        }
+        public Class22(int x)
+        {
+            ch = x; z = 1; 
+        }
+        public Class22(int x, int y, int n)
+        {
+            ch = y*n+x; z = y; 
+        }
+        public double ToDouble() 
+        {
+            return (double)(ch) / z;
+        }
+      public static Class22 operator + (Class22 a, Class22 b)
+        {
+            return new Class22(a.ch + b.ch, a.z);
+        }
+    }
+}
